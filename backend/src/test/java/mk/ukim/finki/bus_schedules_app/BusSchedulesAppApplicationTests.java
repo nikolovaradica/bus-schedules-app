6037753e374f6800905c10970a5ff54a5806b4b6
nikolovaradica <nikolova.radica@gmail.com>
2026-03-11 21:33:53 +0100

    disable security in tests for ci

--- backend/src/test/java/mk/ukim/finki/bus_schedules_app/BusSchedulesAppApplicationTests.java
+++ backend/src/test/java/mk/ukim/finki/bus_schedules_app/BusSchedulesAppApplicationTests.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
-@SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
+@SpringBootTest(
+        properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"
+)
 class BusSchedulesAppApplicationTests {
 
     @Test
