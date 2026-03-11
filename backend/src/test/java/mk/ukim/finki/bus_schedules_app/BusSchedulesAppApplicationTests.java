7d38077df4c3239fa8216e3350a4025ddbe90e78
nikolovaradica <nikolova.radica@gmail.com>
2026-03-11 21:56:32 +0100

    modify be test

--- backend/src/test/java/mk/ukim/finki/bus_schedules_app/BusSchedulesAppApplicationTests.java
+++ backend/src/test/java/mk/ukim/finki/bus_schedules_app/BusSchedulesAppApplicationTests.java
@@ -1,12 +1,9 @@
 package mk.ukim.finki.bus_schedules_app;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;\
 
-@SpringBootTest(
-        properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"
-)
+@SpringBootTest
 class BusSchedulesAppApplicationTests {
 
     @Test
