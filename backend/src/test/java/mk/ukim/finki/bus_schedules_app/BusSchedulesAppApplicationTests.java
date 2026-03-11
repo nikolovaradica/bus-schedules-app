bad29b26fc6735d3c9da0d54becf405bc153e90b
nikolovaradica <nikolova.radica@gmail.com>
2026-03-11 21:24:57 +0100

    disable security filters in test

--- backend/src/test/java/mk/ukim/finki/bus_schedules_app/BusSchedulesAppApplicationTests.java
+++ backend/src/test/java/mk/ukim/finki/bus_schedules_app/BusSchedulesAppApplicationTests.java
@@ -2,8 +2,10 @@ package mk.ukim.finki.bus_schedules_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
 class BusSchedulesAppApplicationTests {
 
     @Test
