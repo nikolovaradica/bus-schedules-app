6a405b1772975afc32ba59b3fd6293ef2839f7d8
nikolovaradica <nikolova.radica@gmail.com>
2026-03-11 21:57:54 +0100

    small linting fix

--- backend/src/test/java/mk/ukim/finki/bus_schedules_app/BusSchedulesAppApplicationTests.java
+++ backend/src/test/java/mk/ukim/finki/bus_schedules_app/BusSchedulesAppApplicationTests.java
@@ -1,7 +1,7 @@
 package mk.ukim.finki.bus_schedules_app;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;\
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BusSchedulesAppApplicationTests {
